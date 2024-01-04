c0e2d6ccb3d8ec80848a7d84c6c9616a2150d541
MichiasShiferaw <mshif073@uottawa.ca>
2024-01-03 21:43:32 -0500

    VIsualize department

---
+++ src/app/[empId]/nurse/visualize_depart/page.tsx
@@ -0,0 +1,10 @@
+import VisualizeDepartment from '@/app/_components/visualizeManagement/visualizeDepartment'
+import React from 'react'
+
+const page = () => {
+  return (
+    <VisualizeDepartment divisionName={"Cardiology"}/>
+  )
+}
+
+export default page
